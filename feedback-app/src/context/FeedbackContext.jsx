a0ccd1a9790df968306144591ca7024eddaf04a2
melikaakrami64521 <melikaakrami64521@gmail.com>
2023-01-25 13:32:55 +0300

    comment uuid for netlify

--- feedback-app/src/context/FeedbackContext.jsx
+++ feedback-app/src/context/FeedbackContext.jsx
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid'
+// import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState, useEffect } from 'react'
 // import FeedbackData from '../data/FeedbackData'
 
